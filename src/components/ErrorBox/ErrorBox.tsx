639c29adf285f3e518f753371dd1c60e13589495
smartgru25 <mike.nktech@gmail.com>
2023-05-04 21:54:01 +0200

    fix a bug redirecting from signin to homepage

--- src/components/ErrorBox/ErrorBox.tsx
+++ src/components/ErrorBox/ErrorBox.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import styles from './ErrorBox.module.css';
+import { removeToken } from '../../token';
 
 export function ErrorBox() {
   return (
     <div className={`${styles.container} box`}>
-      Invalid token. You need to <Link to="./signin">Sign In</Link> to see the <b>SECRET</b> message.
+      Invalid token. You need to <Link to="./signin" onClick={removeToken}>Sign In</Link> to see the <b>SECRET</b> message.
     </div>
   )
 }
